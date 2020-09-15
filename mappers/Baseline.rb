7ae0f9a33e3c9028422b4d9cb1727d304d2d614e
Shen <dshen109@gmail.com>
2020-09-14 19:53:57 -0700

    mappers.Baseline: style fixes

--- mappers/Baseline.rb
+++ mappers/Baseline.rb
@@ -1,30 +1,30 @@
 #*********************************************************************************
-# URBANopt, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other 
+# URBANopt, Copyright (c) 2019-2020, Alliance for Sustainable Energy, LLC, and other
 # contributors. All rights reserved.
-# 
-# Redistribution and use in source and binary forms, with or without modification, 
+#
+# Redistribution and use in source and binary forms, with or without modification,
 # are permitted provided that the following conditions are met:
-# 
-# Redistributions of source code must retain the above copyright notice, this list 
+#
+# Redistributions of source code must retain the above copyright notice, this list
 # of conditions and the following disclaimer.
-# 
-# Redistributions in binary form must reproduce the above copyright notice, this 
-# list of conditions and the following disclaimer in the documentation and/or other 
+#
+# Redistributions in binary form must reproduce the above copyright notice, this
+# list of conditions and the following disclaimer in the documentation and/or other
 # materials provided with the distribution.
-# 
-# Neither the name of the copyright holder nor the names of its contributors may be 
-# used to endorse or promote products derived from this software without specific 
+#
+# Neither the name of the copyright holder nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific
 # prior written permission.
-# 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 # OF THE POSSIBILITY OF SUCH DAMAGE.
 #*********************************************************************************
 
@@ -37,59 +37,59 @@ require 'json'
 module URBANopt
   module Scenario
     class BaselineMapper < SimulationMapperBase
-    
+
       # class level variables
       @@instance_lock = Mutex.new
       @@osw = nil
       @@geometry = nil
-    
+
       def initialize()
-      
+
         # do initialization of class variables in thread safe way
         @@instance_lock.synchronize do
-          if @@osw.nil? 
+          if @@osw.nil?
 
             # load the OSW for this class
             osw_path = File.join(File.dirname(__FILE__), 'base_workflow.osw')
             File.open(osw_path, 'r') do |file|
               @@osw = JSON.parse(file.read, symbolize_names: true)
             end
-        
+
             # add any paths local to the project
             @@osw[:file_paths] << File.join(File.dirname(__FILE__), '../weather/')
-            
-            # configures OSW with extension gem paths for measures and files, all extension gems must be 
+
+            # configures OSW with extension gem paths for measures and files, all extension gems must be
             # required before this
             @@osw = OpenStudio::Extension.configure_osw(@@osw)
           end
         end
       end
-      
+
       def create_osw(scenario, features, feature_names)
-        
+
         if features.size != 1
           raise "TestMapper1 currently cannot simulate more than one feature"
         end
         feature = features[0]
         feature_id = feature.id
-        feature_type = feature.type 
+        feature_type = feature.type
         feature_name = feature.name
         if feature_names.size == 1
           feature_name = feature_names[0]
         end
-        
+
         # deep clone of @@osw before we configure it
         osw = Marshal.load(Marshal.dump(@@osw))
-        
+
         # now we have the feature, we can look up its properties and set arguments in the OSW
         osw[:name] = feature_name
         osw[:description] = feature_name
-        
+
         if feature_type == 'Building'
 
           # set_run_period
           begin
-            timesteps_per_hour = feature.timesteps_per_hour 
+            timesteps_per_hour = feature.timesteps_per_hour
             if !timesteps_per_hour.empty?
               OpenStudio::Extension.set_measure_argument(osw, 'set_run_period', 'timesteps_per_hour', timesteps_per_hour)
             end
@@ -117,7 +117,7 @@ module URBANopt
             end
           rescue
           end
- 
+
           # convert to hash
           building_hash = feature.to_hash
           # check for detailed model filename
@@ -152,35 +152,35 @@ module URBANopt
             when 'Strip shopping mall'
               building_type_1 = 'RetailStripmall'
             when 'Retail other than mall'
-              building_type_1 = 'RetailStandalone' 
+              building_type_1 = 'RetailStandalone'
             when 'Education'
               building_type_1 = 'SecondarySchool'
             when 'Nursing'
-              building_type_1 = 'MidriseApartment'  
+              building_type_1 = 'MidriseApartment'
             when 'Mixed use'
               mixed_type_1 = building_hash[:mixed_type_1]
               mixed_type_2 = building_hash[:mixed_type_2]
               mixed_type_2_percentage = building_hash[:mixed_type_2_percentage]
               mixed_type_2_fract_bldg_area = mixed_type_2_percentage*0.01
-                         
+
               mixed_type_3 = building_hash[:mixed_type_3]
               mixed_type_3_percentage = building_hash[:mixed_type_3_percentage]
               mixed_type_3_fract_bldg_area = mixed_type_3_percentage*0.01
-  
+
               mixed_type_4 = building_hash[:mixed_type_4]
               mixed_type_4_percentage = building_hash[:mixed_type_4_percentage]
               mixed_type_4_fract_bldg_area = mixed_type_4_percentage*0.01
-  
+
               mixed_use_types = []
               mixed_use_types << mixed_type_1
               mixed_use_types << mixed_type_2
               mixed_use_types << mixed_type_3
               mixed_use_types << mixed_type_4
-  
+
               openstudio_mixed_use_types = []
-  
+
               mixed_use_types.each do |mixed_use_type|
-  
+
                 case mixed_use_type
                 when 'Multifamily (5 or more units)'
                   mixed_use_type = 'MidriseApartment'
@@ -201,21 +201,21 @@ module URBANopt
                 when 'Strip shopping mall'
                   mixed_use_type = 'RetailStripmall'
                 when 'Retail other than mall'
-                  mixed_use_type = 'RetailStandalone' 
+                  mixed_use_type = 'RetailStandalone'
                 when 'Education'
                   mixed_use_type = 'SecondarySchool'
                 when 'Nursing'
-                  mixed_use_type = 'MidriseApartment' 
+                  mixed_use_type = 'MidriseApartment'
                 end
-  
+
                 openstudio_mixed_use_types << mixed_use_type
               end
-  
-              openstudio_mixed_type_1 = openstudio_mixed_use_types[0]  
+
+              openstudio_mixed_type_1 = openstudio_mixed_use_types[0]
               openstudio_mixed_type_2 = openstudio_mixed_use_types[1]
               openstudio_mixed_type_3 = openstudio_mixed_use_types[2]
               openstudio_mixed_type_4 = openstudio_mixed_use_types[3]
-  
+
             end
             footprint_area = building_hash[:footprint_area]
             floor_height = 10
@@ -242,7 +242,6 @@ module URBANopt
               new_time = [hour, minute_fraction].join('.')
               return new_time
             end
-
 
             # ChangeBuildingLocation
             # set skip measure to false change building location
@@ -276,7 +275,7 @@ module URBANopt
               OpenStudio::Extension.set_measure_argument(osw, 'ChangeBuildingLocation', 'weather_file_name', weather_filename)
             end
           rescue
-          end 
+          end
 
             #set weekday start time
             begin
@@ -297,7 +296,7 @@ module URBANopt
               end
             rescue
             end
-            
+
             # set weekend start time
             begin
               weekend_start_time = feature.weekend_start_time
@@ -307,7 +306,7 @@ module URBANopt
               end
             rescue
             end
-            
+
             # set weekend duration
             begin
               weekend_duration = feature.weekend_duration
@@ -328,10 +327,10 @@ module URBANopt
               end
             rescue
             end
-            
+
             # TODO: surface_elevation has no current mapping
             # TODO: tariff_filename has no current mapping
-            
+
             # create a bar building, will have spaces tagged with individual space types given the
             # input building types
             # set skip measure to false
@@ -346,19 +345,19 @@ module URBANopt
             if building_type_1 == 'Mixed use'
 
               OpenStudio::Extension.set_measure_argument(osw, 'create_bar_from_building_type_ratios', 'bldg_type_a', openstudio_mixed_type_1)
-              
+
               OpenStudio::Extension.set_measure_argument(osw, 'create_bar_from_building_type_ratios', 'bldg_type_b', openstudio_mixed_type_2)
-              
+
               OpenStudio::Extension.set_measure_argument(osw, 'create_bar_from_building_type_ratios', 'bldg_type_b_fract_bldg_area', mixed_type_2_fract_bldg_area)
-                        
+
               OpenStudio::Extension.set_measure_argument(osw, 'create_bar_from_building_type_ratios', 'bldg_type_c', openstudio_mixed_type_3)
-              
+
               OpenStudio::Extension.set_measure_argument(osw, 'create_bar_from_building_type_ratios', 'bldg_type_c_fract_bldg_area', mixed_type_3_fract_bldg_area)
-              
+
               OpenStudio::Extension.set_measure_argument(osw, 'create_bar_from_building_type_ratios', 'bldg_type_d', openstudio_mixed_type_4)
-              
-              OpenStudio::Extension.set_measure_argument(osw, 'create_bar_from_building_type_ratios', 'bldg_type_d_fract_bldg_area', mixed_type_4_fract_bldg_area)      
-            
+
+              OpenStudio::Extension.set_measure_argument(osw, 'create_bar_from_building_type_ratios', 'bldg_type_d_fract_bldg_area', mixed_type_4_fract_bldg_area)
+
             end
 
             # calling create typical building the first time will create space types
@@ -366,9 +365,9 @@ module URBANopt
             OpenStudio::Extension.set_measure_argument(osw, 'create_typical_building_from_model', 'add_hvac', false, 'create_typical_building_from_model 1')
 
             # create a blended space type for each story
-            OpenStudio::Extension.set_measure_argument(osw, 
+            OpenStudio::Extension.set_measure_argument(osw,
               'blended_space_type_from_model', '__SKIP__', false)
-            OpenStudio::Extension.set_measure_argument(osw, 
+            OpenStudio::Extension.set_measure_argument(osw,
             'blended_space_type_from_model', 'blend_method', 'Building Story')
 
             # create geometry for the desired feature, this will reuse blended space types in the model for each story and remove the bar geometry
@@ -376,7 +375,7 @@ module URBANopt
             OpenStudio::Extension.set_measure_argument(osw, 'urban_geometry_creation', 'geojson_file', scenario.feature_file.path)
             OpenStudio::Extension.set_measure_argument(osw, 'urban_geometry_creation', 'feature_id', feature_id)
             OpenStudio::Extension.set_measure_argument(osw, 'urban_geometry_creation', 'surrounding_buildings', 'ShadingOnly')
-                          
+
             # call create typical building a second time, do not touch space types, only add hvac
             OpenStudio::Extension.set_measure_argument(osw, 'create_typical_building_from_model', '__SKIP__', false)
             OpenStudio::Extension.set_measure_argument(osw, 'create_typical_building_from_model', 'system_type', system_type, 'create_typical_building_from_model 2')
@@ -387,11 +386,11 @@ module URBANopt
           OpenStudio::Extension.set_measure_argument(osw, 'default_feature_reports', 'feature_id', feature_id)
           OpenStudio::Extension.set_measure_argument(osw, 'default_feature_reports', 'feature_name', feature_name)
           OpenStudio::Extension.set_measure_argument(osw, 'default_feature_reports', 'feature_type', feature_type)
-        end 
+        end
 
         return osw
-      end 
-      
+      end
+
     end #BaselineMapper
   end #Scenario
 end #URBANopt
